bd71d49c7e3d37c1308f0a94c4c6022e72af3b30
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-03 10:28:52 +0300

    Update settings.py

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -1,1 +1,10 @@
 
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
