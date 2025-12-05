fe897fba2ec681f03fdda24386aa05fdd821ef33
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-05 14:42:56 +0300

    Update models.py

--- LibraryProject/BookShelf/models.py
+++ LibraryProject/BookShelf/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    published_year = models.IntegerField()
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
 
