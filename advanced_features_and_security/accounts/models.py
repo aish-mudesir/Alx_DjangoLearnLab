fa3b831732e1bc528c3265e8b85503d4dd41a14c
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-05 14:17:24 +0300

    Update models.py

--- advanced_features_and_security/accounts/models.py
+++ advanced_features_and_security/accounts/models.py
@@ -1,3 +1,41 @@
+
+
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
