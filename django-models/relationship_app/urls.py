b09fce20639418b598166d08b7ccec5e4670a2ef
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-05 09:48:50 +0300

    Revise URL patterns for book management
    Updated URL patterns to include book management views.

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,19 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
-]
-
-
-
-
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import add_book, edit_book, delete_book
 
 urlpatterns = [
-    path("books/", list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
 
