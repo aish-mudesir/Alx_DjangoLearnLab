a4772f487ae8eed64c8158b24eb0477db441ccd7
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-05 14:43:31 +0300

    Update views.py

--- LibraryProject/BookShelf/views.py
+++ LibraryProject/BookShelf/views.py
@@ -1,1 +1,10 @@
 
+from django.shortcuts import render
+from .models import Book
+
+def index(request):
+    return render(request, 'BookShelf/index.html')
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'BookShelf/book_list.html', {'books': books})
