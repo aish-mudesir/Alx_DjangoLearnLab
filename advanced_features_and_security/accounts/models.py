c73ba1758ca005330dc3f407edf6a87d342bcfa4
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-05 15:04:36 +0300

    Update models.py

--- advanced_features_and_security/accounts/models.py
+++ advanced_features_and_security/accounts/models.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
     """Custom manager for CustomUser model"""
-
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
@@ -16,10 +16,6 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
@@ -30,3 +26,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = (
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        )
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
