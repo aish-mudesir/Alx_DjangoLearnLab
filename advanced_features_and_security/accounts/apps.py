a52cdd203f17f6774dfa7ef7c24e062032fab093
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-08 15:12:46 +0300

    Update apps.py

--- advanced_features_and_security/accounts/apps.py
+++ advanced_features_and_security/accounts/apps.py
@@ -1,1 +1,13 @@
 
+from django.apps import AppConfig
+from django.contrib.auth.models import Group, Permission
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'accounts'
+
+    def ready(self):
+        # Create groups and assign permissions
+        Group.objects.get_or_create(name='Editors')
+        Group.objects.get_or_create(name='Viewers')
+        Group.objects.get_or_create(name='Admins')
