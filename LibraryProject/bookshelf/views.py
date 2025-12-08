6076fbf44da26c081414ce4bc11a3cf81753a2d9
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-08 16:03:02 +0300

    Create views.py

---
+++ LibraryProject/bookshelf/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from .models import Book
+
+def home(request):
+    books = Book.objects.all()
+    output = ", ".join([b.title for b in books])
+    return HttpResponse(f"Books: {output}")
