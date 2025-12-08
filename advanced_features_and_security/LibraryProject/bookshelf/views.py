df3cc11b57eff9710e24411da62cf93f5bc5988c
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-08 16:20:39 +0300

    Create views.py

---
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
+from .models import Book
+
+# Function-based view: list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/list_books.html', {'books': books})
+
+# Secured views with permission decorators
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return HttpResponse("You have permission to create a book!")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    return HttpResponse(f"You have permission to edit book ID {book_id}!")
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    return HttpResponse(f"You have permission to delete book ID {book_id}!")
