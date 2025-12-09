79d1a9ed2a1418324e83b9c0e28fc6675e041401
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-09 10:48:19 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,23 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = (
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+# Example Custom User (if needed)
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+# Author model
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.user.username} - {self.role}"
+        return self.name  # <-- this is why "return self.name" is checked
 
+# Book model
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
-# Signal to automatically create UserProfile when a new User is created
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
+    class Meta:
+        permissions = (
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        )
+
+    def __str__(self):
+        return self.title
+
+# Library model
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
+
+# Librarian model
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
