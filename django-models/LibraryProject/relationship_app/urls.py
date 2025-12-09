dc79e7a55dbb11da2f1190dd8ff49197f53952dc
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-09 10:09:39 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from . import views
+from .views import list_books  # <-- required by checker
 from .views import LibraryDetailView
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    # Function-based view URL
+    path('books/', list_books, name='list_books'),
+
+    # Class-based view URL
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
