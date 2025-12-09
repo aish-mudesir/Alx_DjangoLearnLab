cd0dd92cba5fdbe6bee56824a18caeb549dabd8f
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-09 10:52:32 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,35 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import Book, Library
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.models import User
+from .models import UserProfile
+
+# Helper functions to check user roles
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# Admin view
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html', {})
+
+# Librarian view
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html', {})
+
+# Member view
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html', {})
+
 # For other views if needed
 # Registration view
 def register(request):
