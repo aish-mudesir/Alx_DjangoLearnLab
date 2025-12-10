f72f72ecedc809ba4a6a461c83de943bb0b38a43
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 14:09:32 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,6 +2,45 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+# accounts/serializers.py
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserRegistrationSerializer(serializers.Serializer):
+    """
+    Serializer for user registration.
+    Uses explicit serializers.CharField() and get_user_model().objects.create_user
+    """
+    username = serializers.CharField(max_length=150, required=True)
+    email = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return attrs
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        # ALX check requires get_user_model().objects.create_user
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    """
+    Serializer for user login.
+    """
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
 
 User = get_user_model()
 
