2d0fd05d89718f83aaa482d6cacc33af15865219
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 12:15:44 +0300

    Create settings.py

---
+++ advanced-api-project/settings.py
@@ -0,0 +1,24 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',  # DRF
+    'api',             # Your app
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
