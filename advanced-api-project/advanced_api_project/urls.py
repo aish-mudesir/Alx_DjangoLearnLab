bd62ad07b5ad0d0fa0a1850318415af711d9c0ff
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 13:06:49 +0300

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include  # include is required
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # include api app URLs
 ]
+
