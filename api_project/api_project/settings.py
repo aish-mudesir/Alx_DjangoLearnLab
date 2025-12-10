de27c5e586e758b28a9311e0756e66afa7d9ccb9
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 12:12:04 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -11,4 +11,13 @@ INSTALLED_APPS = [
     'api',                      # Your app
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 
