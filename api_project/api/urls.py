db9a6e3ccc8eadb2653dbeb0ac5b4f115cc33194
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 12:08:13 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from .views import BookList
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
 
+# Create DRF router
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
+# URL patterns
 urlpatterns = [
+    
+    path('', include(router.urls)),                        
     path('books/', BookList.as_view(), name='book-list'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Adds API routes
