e5217663c03ad36ce37efa5eee642c468c051670
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 13:59:11 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
-# accounts/serializers.py
+
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.password_validation import validate_password
 
 User = get_user_model()
@@ -8,15 +9,16 @@ User = get_user_model()
 class UserRegistrationSerializer(serializers.ModelSerializer):
     """
     Serializer for registering a new user.
-    Validates password and creates the user instance.
+    Automatically creates an auth token for the user.
     """
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    token = serializers.CharField(read_only=True)  # Token will be generated on creation
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture']
-    
+        fields = ['username', 'email', 'password', 'password2', 'bio', 'profile_picture', 'token']
+
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
             raise serializers.ValidationError({"password": "Password fields didn't match."})
@@ -24,18 +26,21 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
+        # Create token for the user
+        Token.objects.create(user=user)
         return user
 
 class UserLoginSerializer(serializers.Serializer):
     """
-    Serializer for user login.
+    Serializer for logging in a user.
+    Returns token on successful authentication.
     """
     username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True, write_only=True)
+    password = serializers.CharField(write_only=True, required=True)
