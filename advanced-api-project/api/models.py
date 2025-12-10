31fa26892ebfd4917ee421ee9aba499e44217aa3
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 12:25:24 +0300

    Update models.py

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 from django.utils import timezone
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)  # Author's name
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 # Author model
 class Author(models.Model):
