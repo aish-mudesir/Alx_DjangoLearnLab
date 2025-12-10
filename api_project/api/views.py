9059f7d82849219d7d1cc92b01afa6ae68a8a0c5
Aisha-Amru-Mudesir <139975048+aish-mudesir@users.noreply.github.com>
2025-12-10 12:05:03 +0300

    Update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,18 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+urlpatterns = [
+    path('books/', BookList.as_view(), name='book-list'),  # Original list endpoint
+    path('', include(router.urls)),                        # All CRUD routes
+]
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
